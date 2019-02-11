2ad1ea1cec1c8b764f88aa2d29bffdd4a192de6d
Leo <lianzhong888999@163.com>
2019-02-11 15:06:14 +0800

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/dell/LoGoLoam_ws/src
+# Build directory: /home/dell/LoGoLoam_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(LeGO-LOAM/cloud_msgs)
+subdirs(LeGO-LOAM/LeGO-LOAM)
---
+++ build/LeGO-LOAM/cloud_msgs/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dell/LoGoLoam_ws/src/LeGO-LOAM/cloud_msgs
+# Build directory: /home/dell/LoGoLoam_ws/build/LeGO-LOAM/cloud_msgs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
